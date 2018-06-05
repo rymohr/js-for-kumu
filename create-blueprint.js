ec99c848031cf7c96ff467b258add7241b0e5add
AlexVipond <alex.r.vipond@gmail.com>
2018-06-05 17:39:21 -0500

    indent

--- create-blueprint.js
+++ create-blueprint.js
@@ -1,6 +1,6 @@
 function createBlueprint(elementsArray, connectionsArray) {
-    let blueprint = JSON.parse(
-        "{\"elements\":" + JSON.stringify(elementsArray) + ",\"connections\":" + JSON.stringify(connectionsArray) + "}"
-        );
-    return JSON.stringify(blueprint, null, 2);
+  let blueprint = JSON.parse(
+    "{\"elements\":" + JSON.stringify(elementsArray) + ",\"connections\":" + JSON.stringify(connectionsArray) + "}"
+    );
+  return JSON.stringify(blueprint, null, 2);
 }
