abd72765e78a92cb0f9eee2e5d8ad5b8246a1778
Ryan Mohr <ryan.mohr@gmail.com>
2018-11-15 12:52:20 -1000

    update entity descriptions too and execute via transaction

--- add-markdown-header-spaces.js
+++ add-markdown-header-spaces.js
@@ -6,16 +6,38 @@ function addSpaceToMatch(match, start, leadingSpaces, blockquote, headers) {
 
 function addSpacesToDescription(description) {
   let regexp = /(^|\n)( {0,3})(> {0,4})?(#+)/g
-
   return description.replace(regexp, addSpaceToMatch)
 }
 
-function fixMaps(Maps) {
-  Maps.models.forEach(model => {
-    let fixedDescription = addSpacesToDescription(model.attributes.description)
+function fixDescription(description) {
+  return description ? addSpacesToDescription(description) : description;
+}
 
-    model.save({ description: fixedDescription })
+function fixMaps(transaction) {
+  Maps.each(model => {
+    let description = model.get("description");
+    let descriptionFixed = fixDescription(description);
+    
+    if (description != descriptionFixed) {
+      transaction.update(model, {description: descriptionFixed});
+    }
   })
 }
 
-fixMaps(Maps)
+function fixEntities(transaction) {
+  [Elements, Connections, Loops].forEach(collection => {
+    collection.each(entity => {
+      let description = entity.getAttr("description");
+      let descriptionFixed = fixDescription(description);
+      
+      if (description != descriptionFixed) {
+        transaction.update(entity, () => entity.setAttr("description", descriptionFixed));
+      }
+    })
+  })
+}
+
+transaction = new Transaction()
+fixMaps(transaction)
+fixEntities(transaction)
+transaction.execute()
